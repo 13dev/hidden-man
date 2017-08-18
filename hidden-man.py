50756e4d14cf6900bf623226c0ec5030f33a4891
13dev <qwerty124563@gmail.com>
2017-08-18 19:45:28 +0100

    Clean code, check if process have init edit logo

--- hidden-man.py
+++ hidden-man.py
@@ -38,28 +38,22 @@ if not os.geteuid() == 0:
     sys.exit(1)
 
 c = Color()
-count = 0
 logFile = "hiddenman.log"
-version = "v1.0"
+version = "v1.1"
 
-def checkOpenVPN():
-	global count
-	#t = threading.Timer(5.0, checkOpenVPN)
-	#t.start()
-	(_, outputTun) = commands.getstatusoutput("ifconfig | grep tun | awk '{print $1}'")
-	(_, outputPidof) = commands.getstatusoutput("pidof -x openvpn")
-
-	if(not outputTun):
-		count += 1
-
-	#print outputTun
-	#print count
-
-	if(count == 6):
-		print c.out("%s[-]%s Error connecting VPN check ~/openvpn/%s." %(c.RED, c.RESET, logFile))
-		#t.cancel()
-		stopAll()
-		sys.exit()
+def check(command, commandToVerify, errorMessage = "", successMessage = ""):
+	process = Popen(command, shell=True)
+	while True:
+		if process.poll() is not None:
+			(_, outputCommand) = commands.getstatusoutput(commandToVerify)
+			if(not outputCommand):
+				#Error init the command
+				if(errorMessage):	print errorMessage
+				sys.exit()
+			else:
+				#success init command
+				if(successMessage):	print successMessage
+			break
 
 def stopAll():
 	os.system("ps -uax | grep log- | awk '{print $2}' | xargs kill -9 2> /dev/null")
@@ -75,40 +69,39 @@ def checkProccess():
 def downloadVPN(url, file):
 	sleep(1)
 	if(not os.path.isfile(expanduser("~") + "/openvpn/" + file)):
-		print c.out("%s[+]%s Downloading required files." %(c.GREEN , c.RESET + c.ON_CYAN))
+		print c.out("%s[+] %sDownloading required files." %(c.GREEN , c.RESET + c.ON_CYAN))
 		sleep(1.5)
 		os.system("wget --output-document='temp.zip' " + url)
+
+		#if temp.zip exists then move it.
 		os.system("[ -f temp.zip ] && mv temp.zip ~/openvpn/")
 
 		# extract only selected file 'vpnbook-de-tcp443.ovpn'
 		sleep(1)
-		print c.out("%s[+]%s Unzip downloaded file." %(c.GREEN, c.RESET + c.ON_CYAN))
+		print c.out("%s[+] %sUnzip downloaded file." %(c.GREEN, c.RESET + c.ON_CYAN))
 		sleep(1)
 		os.system("[ -e ~/openvpn/temp.zip ] && unzip -o ~/openvpn/temp.zip '" + file + "' -d ~/openvpn/")
 
 		#remove downloaded file
 		sleep(1.5)
-		print c.out("%s[+]%s Removing files." %(c.GREEN, c.RESET + c.ON_CYAN))
+		print c.out("%s[+] %sRemoving files." %(c.GREEN, c.RESET + c.ON_CYAN))
 		os.system("[ -e ~/openvpn/temp.zip ] && rm ~/openvpn/temp.zip")
 	else:
-		print c.out("%s[+]%s File has already been downloaded, no download required." %(c.GREEN , c.RESET + c.ON_CYAN))
+		print c.out("%s[+] %sFile has already been downloaded, no download required." %(c.GREEN , c.RESET + c.ON_CYAN))
 
 def processVPNChoice(choice):
 
 	if(choice == 'de'):
-
 		downloadFile 	= "https://www.vpnbook.com/free-openvpn-account/VPNBook.com-OpenVPN-DE1.zip"
 		zipFile 		= "vpnbook-de233-tcp443.ovpn"
 		nameChoice		= "Germany"
 
 	elif(choice == 'eu2'):
-
 		downloadFile 	= "https://www.vpnbook.com/free-openvpn-account/VPNBook.com-OpenVPN-Euro2.zip"
 		zipFile 		= "vpnbook-euro2-tcp443.ovpn"
 		nameChoice		= "Europe"
 
 	elif(choice == 'eu1'):
-
 		downloadFile 	= "https://www.vpnbook.com/free-openvpn-account/VPNBook.com-OpenVPN-Euro1.zip"
 		zipFile 		= "vpnbook-euro1-tcp443.ovpn"
 		nameChoice		= "Europe"
@@ -119,35 +112,25 @@ def processVPNChoice(choice):
 		nameChoice		= "Canada"
 
 	elif(choice == 'us2'):
-
 		downloadFile 	= "https://www.vpnbook.com/free-openvpn-account/VPNBook.com-OpenVPN-US2.zip"
 		zipFile 		= "vpnbook-us2-tcp443.ovpn"
 		nameChoice		= "United-states"
 
 	elif(choice == 'us1'):
-
 		downloadFile 	= "https://www.vpnbook.com/free-openvpn-account/VPNBook.com-OpenVPN-US1.zip"
 		zipFile 		= "vpnbook-us1-tcp443.ovpn"
 		nameChoice		= "United-states"
 
 	else:
-		print c.out("%s[-]%s Not valid answer." %(c.RED, c.RESET))
+		print c.out("%s[-] %sNot valid answer." %(c.RED, c.RESET))
 		return
 
 	#downloading files...
 	downloadVPN(downloadFile, zipFile)
 
-
-	#check if process if alredy running
-	(_, outputPidof) = commands.getstatusoutput("pidof -x openvpn")
-	if(outputPidof.strip()):
-		print c.out("%s[-]%s Error connecting VPN check ~/openvpn/%s." %(c.RED, c.RESET, logFile))
-		stopAll()
-		sys.exit()
-
 	#start vpn
 	sleep(2)
-	print c.out("%s[+]%s Connecting to %s server." %(c.GREEN, c.RESET + c.ON_CYAN, nameChoice))
+	print c.out("%s[+] %sConnecting to %s server." %(c.GREEN, c.RESET + c.ON_CYAN, nameChoice))
 	sleep(1)
 
 	#add configure the ovpn file...
@@ -158,10 +141,12 @@ def processVPNChoice(choice):
 	os.system("cat /dev/null > ~/openvpn/" + logFile)
 
 	#start
-	os.system("nohup openvpn --cd ~/openvpn --config " + zipFile + " --auth-user-pass ./authfile > ~/openvpn/" + logFile + " 2>&1 &")
-	print c.out("%s[*]%s Always check log file in: ~/openvpn/%s." %(c.YELLOW, c.RESET, logFile))
-
-	checkOpenVPN()
+	#os.system("nohup openvpn --cd ~/openvpn --config " + zipFile + " --auth-user-pass ./authfile > ~/openvpn/" + logFile + " 2>&1 &")
+	#print c.out("%s[*]%s Always check log file in: ~/openvpn/%s." %(c.YELLOW, c.RESET, logFile))
+	check(["nohup openvpn --cd %s --config %s --auth-user-pass %s > %s 2>&1 &" %('~/openvpn', zipFile,'./authfile', '~/openvpn/' + logFile)], 
+		'pidof -x openvpn',
+		c.out("%s[-] %sError connecting VPN check ~/openvpn/%s." %(c.RED, c.RESET, logFile)),
+		c.out("%s[+] %sConnected to %s VPN." %(c.GREEN, c.RESET + c.ON_GREEN, nameChoice)))
 
 def getRandomMac():
 	'''Returns a random MAC''' 
@@ -238,15 +223,15 @@ def hideIP(hide):
 	print("\n")
 
 	try:
-		destination = raw_input(c.BLUE + c.BOLD +'Choice >' + c.RESET).lower().strip()
+		destination = raw_input(c.BLUE + c.BOLD +'Choice > ' + c.RESET).lower().strip()
 		processVPNChoice(destination)
 	except KeyboardInterrupt:
 		sys.exit(c.RED + c.BOLD + "\nChoice cancelled...")
 
 parser = argparse.ArgumentParser(description='Hide your entity, Hidden-Man.')
 parser.add_argument('--hidemac', help = 'Hide you MAC-Address.',required=False, action='store_true',default=False)
-parser.add_argument('--restoremac', help = 'Set to default MAC-Address..',required=False, action='store_true',default=False)
-parser.add_argument('--hideip', help = 'Hide you IP.',required=False, action='store_true',default=False)
+parser.add_argument('--restoremac', help = 'Set to default MAC-Address.',required=False, action='store_true',default=False)
+parser.add_argument('--hideip', help = 'Hide you IP-Address using VPN.',required=False, action='store_true',default=False)
 parser.add_argument('--forcestop', help = 'Force to stop all process.',required=False, action='store_true',default=False)
 
 args = parser.parse_args()
@@ -255,18 +240,16 @@ logo = u"""
      _  _ ___ ___  ___  ___ _  _     __  __   _   _  _ \N{COPYRIGHT SIGN}
     | || |_ _|   \|   \| __| \| |___|  \/  | /_\ | \| |
     | __ || || |) | |) | _|| .` |___| |\/| |/ _ \| .` |
-    |_||_|___|___/|___/|___|_|\_|   |_|  |_/_/ \_\_|\_|""" + version + """
+    |_||_|___|___/|___/|___|_|\_|   |_|  |_/_/ \_\_|\_| """ + version + """
     
-    Developed by 13dev @o_psy__ - qwerty124563@gmail.com
-
 """
-
 for char in logo:
-    sleep(.004)
-    sys.stdout.write(c.RED)
-    sys.stdout.write(char)
-    sys.stdout.flush()
+	sleep(.004)
+	sys.stdout.write(c.RED)
+	sys.stdout.write(char)
+	sys.stdout.flush()
 
+print c.out("    %s> DEVELOPED BY 13DEV (qwerty124563@gmail.com)" %( c.GREEN))
 sys.stdout.write(c.RESET)
 sleep(.5)
 
